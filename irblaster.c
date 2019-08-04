7168cd5eb408930679a306d045d4c4e8ddcaf088
illuminati1911 <ville.pv@gmail.com>
2019-08-04 06:53:39 +0000

    Refactoring and added logging macros.

--- irblaster.c
+++ irblaster.c
@@ -2,14 +2,10 @@
 #include <linux/module.h>
 #include <linux/device.h>
 #include <linux/kernel.h>
+#include <linux/fs.h>
 
 #define DEVICE_NAME "irblaster"
 #define CLASS_NAME "ir"
-
-#define PWM_CONTROL_OFFSET 0
-#define PWM_STATUS_OFFSET  1
-#define PWM0_RANGE_OFFSET  4
-#define PWM0_DATA_OFFSET   5
 
 #define BCM2835_P_BASE   0x3F000000
 #define LINUX_BLOCK_SIZE  (4 * 1024)
@@ -19,10 +15,19 @@
 #define CLK_CNTL_OFFSET     40
 #define CLK_DIV_OFFSET      41
 
+#define PWM_CONTROL_OFFSET 0
+#define PWM_STATUS_OFFSET  1
+#define PWM0_RANGE_OFFSET  4
+#define PWM0_DATA_OFFSET   5
+
 // Set GPIO as input or zero out the function selector. 
 //
 #define INP_GPIO(g) *(base_gpio+((g )/10)) &= ~(7<<(((g)%10)*3))
 #define SET_GPIO_ALT(g,a) *(base_gpio+(((g)/10))) |= (((a)<=3?(a)+4:((a)==4?3:2))<<(((g)%10)*3))
+
+#define log(...) printk(KERN_INFO "irblaster: " __VA_ARGS__);
+#define alert(...) printk(KERN_ALERT "irblaster: Warning - " __VA_ARGS__);	
+
 
 MODULE_LICENSE("Dual MIT/GPL");
 MODULE_AUTHOR("illuminati1911");
@@ -32,11 +37,27 @@ MODULE_VERSION("0.1");
 static int major_number;
 static char message[300] = {0};
 static unsigned int ir_gpio = 18;
-static struct class* irblaster_class = NULL;
-static struct device* irblaster_device = NULL;
+static struct class* ib_class = NULL;
+static struct device* ib_device = NULL;
 // Mutex to control/limit access to the device.
 //
-static DEFINE_MUTEX(irblaster_mutex);
+static DEFINE_MUTEX(ib_mutex);
+
+// Function definitions
+//
+static int dev_open(struct inode *, struct file *);
+static int dev_release(struct inode *, struct file *);
+static ssize_t dev_read(struct file *, char *, size_t, loff_t *);
+static ssize_t dev_write(struct file *, const char *, size_t, loff_t *);
+
+// Character driver operations.
+//
+static struct file_operations fops = {
+    .open = dev_open,
+    .read = dev_read,
+    .write = dev_write,
+    .release = dev_release,
+};
 
 /*  BCM2835 Device registers:
 *
@@ -70,14 +91,66 @@ static unsigned *pwm_dat1 = 0;
 static unsigned *clk_cntl = 0;
 static unsigned *clk_div = 0;
 
-static int __init irblaster_init(void) {
-    printk(KERN_INFO "irblaster: Initializing driver...\n");
+static int __init ib_init(void) {
+    log("Initializing driver...");
+
+    // Dynamic allocation of major number for the device.
+    //
+    major_number = register_chrdev(0, DEVICE_NAME, &fops);
+    if (major_number < 0) {
+        alert("Failed to register a major number!");
+        return major_number;
+    }
+    log("Chrdev registered correctly with major number %d", major_number);
+
+    // Register device class 
+    //
+    ib_class = class_create(THIS_MODULE, CLASS_NAME);
+    if (IS_ERR(ib_class)) {
+        unregister_chrdev(major_number, DEVICE_NAME);
+        log("Failed to register device class!");
+        return PTR_ERR(ib_class);
+    }
+    log("Device class registered correctly.");
+
+    // Register device driver
+    //
+    ib_device = device_create(ib_class, NULL, MKDEV(major_number, 0), NULL, DEVICE_NAME);
+    if (IS_ERR(ib_device)) {
+        class_destroy(ib_class);
+        unregister_chrdev(major_number, DEVICE_NAME);
+        alert("Failed to create device!");
+        return PTR_ERR(ib_device);
+    }
+
+    log("Device class created successfully!");
+    mutex_init(&ib_mutex);
     return 0;
 }
 
-static void __exit irblaster_exit(void) {
-    printk(KERN_INFO "irblaster: Driver closing... Bye!\n");
+static void __exit ib_exit(void) {
+    device_destroy(ib_class, MKDEV(major_number, 0));
+    class_destroy(ib_class);
+    unregister_chrdev(major_number, DEVICE_NAME);
+    mutex_destroy(&ib_mutex);
+    log("Driver closing... Bye!");
 }
 
-module_init(irblaster_init);
-module_exit(irblaster_exit);
+static int dev_open(struct inode *inodep, struct file *filep) {
+    return 0;
+}
+
+static int dev_release(struct inode *inodep, struct file *filep) {
+    return 0;
+}
+
+static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *offset) {
+    return 0;
+}
+
+static ssize_t dev_write(struct file *filep, const char *buffer, size_t len, loff_t *offset) {
+    return 0;
+}
+
+module_init(ib_init);
+module_exit(ib_exit);
